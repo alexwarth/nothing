2b28fe65c4f3b16509dd7d8307c013e11f099c52
Alessandro Warth <awarth@Monad.local>
2010-04-28 15:58:16 -0700

    cleaned up the code, got closures working 100%

--- bmc.js
+++ bmc.js
@@ -1,65 +1,87 @@
 ometa BMLParser <: Parser {
-  ach  = ~('(' | ')' | space) char,
-  atom = spaces <ach+>:x            -> ["atom", x],
-  num  = spaces <digit+>:x          -> ["num",  x],
-  expr = num
-       | atom
-       | "(" expr*:xs ")"           -> ["expr", xs]
+  atomChar = ~('(' | ')' | space) char,
+  atom     = spaces <atomChar+>:x          -> ["atom", x],
+  num      = spaces <digit+>:x             -> ["num",  x],
+  expr     = num | atom | "(" expr*:xs ")" -> ["expr", xs],
+  parse    = expr:tree spaces end          -> tree
 }
 
 ometa BMLCompiler {
-  compile = comp -> this.makeOutput(),
-  comp    = ['num' anything:n]                               -> this.output.push("PUSH(" + n + ")")
-          | ['atom' anything:n]                              -> this.output.push("ARG(" + this.symtab()[n] + ")")
-          | ['expr' [['atom' 'unless'] comp
-                       {this.output.length}:jnzIdx
-                       {this.output.push(null)}
-                       comp
-                       {this.output.push("RET")}
-                       {this.output[jnzIdx] = "JNZ(" + (this.output.length - jnzIdx - 1) + ")"}
-                       comp]]
-          | ['expr' [['atom' '+']      comp comp]]           -> this.output.push("ADD")
-          | ['expr' [['atom' '-']      comp comp]]           -> this.output.push("SUB")
-          | ['expr' [['atom' '*']      comp comp]]           -> this.output.push("MUL")
-          | ['expr' [['atom' 'lambda'] lambdaArgsAndBody]]
-          | ['expr' [comp boxArg (&anything comp boxArg)*:args]] -> this.output.push("CALL(" + args.length + ")")
-          | {throw "compilation failed"},
-  boxArg  = {this.output.push("BOX")},
-
-  lambdaArgsAndBody = {this.output}:oldOutput
-                        {this.output = ["nil"]; this.symtabs.push({_numVars: 0}); this.addArg("thisFunction")}
-                        ['expr' [(['atom' :a] -> this.addArg(a))*]]
-                        comp
-                        {this.output.push("RET");
-                         this.symtabs.pop();
-                         this.lambdas.push(this.output);
-                         this.output = oldOutput;
-                         this.output.push("mk2(Int(IPUSH), l" + this.lambdas.length + ")")}
+  compile          = comp                                            -> this.makeOutput(),
+  comp             = ['num' anything:n]                                 emit("Push(" + n + ")")
+                   | ['atom' anything:n]                                emit(this.lookup(n)) emit("Unbox")
+                   | ['expr' [['atom' 'if']     comp
+                              blankInstr:jzIdx  comp                    emitAt(jzIdx,  "JZ("  + (this.ic() - jzIdx)      + ")")
+                              blankInstr:jmpIdx comp             ]]     emitAt(jmpIdx, "Jmp(" + (this.ic() - jmpIdx - 1) + ")")
+                   | ['expr' [['atom' '='     ] comp comp        ]]     emit("Eq")
+                   | ['expr' [['atom' '+'     ] comp comp        ]]     emit("Add")
+                   | ['expr' [['atom' '-'     ] comp comp        ]]     emit("Sub")
+                   | ['expr' [['atom' '*'     ] comp comp        ]]     emit("Mul")
+                   | ['expr' [['atom' 'lambda'] lambda           ]]
+                   | ['expr' [comp box (&anything comp box)*:args]]     emit("Call(" + args.length + ")")
+                   | { throw "compilation failed" },
+  blankInstr       = emit(null)                                      -> (this.level().out.length - 1),
+  emit        :ins =                                                 -> this.level().out.push(ins),
+  emitAt :idx :ins =                                                 -> (this.level().out[idx] = ins),
+  box              =                                                    emit("Box"),
+  lambda           = { this.levels.push({fvs: [],
+                                         syms: {_numVars: 0},
+                                         out: []})
+                       this.addArg("thisFunction")
+                       this.level()                          }:level
+                     ['expr' [(['atom' :a] -> this.addArg(a))*]] comp   emit("Ret")
+                     { this.levels.pop()
+                       this.lambdas.push(level.out) }                   emit("mk2(Int(oPush), l" + this.lambdas.length + ")")
+                     { for (var i = 0; i < level.fvs.length; i++)       this._applyWithArgs("emit", this.lookup(level.fvs[i])) }
+                                                                        emit("MkFun(" + level.fvs.length + ")")
 }
-BMLCompiler.initialize = function() {
-  this.symtabs = []
-  this.output = []
-  this.lambdas = []
-}
-BMLCompiler.symtab = function() { return this.symtabs[this.symtabs.length - 1] }
-BMLCompiler.addArg = function(a) {
-  this.symtab()[a] = this.symtab()._numVars++
-}
+BMLCompiler.initialize  = function()      { this.levels = [{fvs: [], syms: {_numVars: 0}, out: []}]
+                                            this.lambdas = [] }
+BMLCompiler.level       = function()      { return this.levels[this.levels.length - 1] }
+BMLCompiler.ic          = function()      { return this.level().out.length }
+BMLCompiler.addArg      = function(a)     { this.level().syms[a] = this.level().syms._numVars++ }
+BMLCompiler.lookup      = function(n)     { var li = this.levels.length - 1
+                                            while (li > 0) {
+                                              var vi = this.levels[li].syms[n]
+                                              if (vi != undefined) {
+                                                if (this.levels[li] == this.level())
+                                                  return "Arg(" + vi + ")"
+                                                while (++li < this.levels.length)
+                                                  vi = this.addFv(li, n)
+                                                return "FV(" + vi + ")"
+                                              }
+                                              li--
+                                            }
+                                            throw "undeclared variable " + n }
+BMLCompiler.addFv       = function(li, n) { var fvs = this.levels[li].fvs
+                                            for (var i = 0; i < fvs.length; i++)
+                                              if (fvs[i] == n)
+                                                return i
+                                            fvs.push(n)
+                                            return fvs.length - 1 }
 BMLCompiler.iMakeOutput = function(name, instrs, ws) {
-    ws.nextPutAll("value_t " + name + " = mk(" + instrs.length + ");\n")
-    ws.nextPutAll("addGlobal(" + name + ");\n");
-    for (var i = 0; i < instrs.length; i++)
-      ws.nextPutAll("slotAtPut(" + name + ", Int(" + i + "), " + instrs[i] + ");\n")
-}
-BMLCompiler.makeOutput = function() {
-  var ws = new StringBuffer()
-  for (var i = 0; i < this.lambdas.length; i++)
-    this.iMakeOutput("l" + (i + 1), this.lambdas[i], ws)
-  this.output.push("HALT")
-  this.iMakeOutput("prog", this.output, ws)
-  return ws.contents()
-}
+                                            ws.nextPutAll("value_t " + name + " = mk(" + instrs.length + ");\n")
+                                            ws.nextPutAll("addGlobal(" + name + ");\n");
+                                            for (var i = 0; i < instrs.length; i++)
+                                              ws.nextPutAll("slotAtPut(" + name + ", Int(" + i + "), " + instrs[i] + ");\n") }
+BMLCompiler.makeOutput  = function()      { var ws = new StringBuffer()
+                                            for (var i = 0; i < this.lambdas.length; i++)
+                                              this.iMakeOutput("l" + (i + 1), this.lambdas[i], ws)
+                                            this.level().out.push("Halt")
+                                            this.iMakeOutput("prog", this.level().out, ws)
+                                            return ws.contents() }
 
-tree = BMLParser.matchAll("((lambda (n) (unless n 1 (* n (thisFunction (- n 1))))) 5)", "expr")
-//tree = BMLParser.matchAll("((lambda (x) (+ x 1)) 5)", "expr")
-code = BMLCompiler.match(tree, "compile") 
+//tree = BMLParser.matchAll("((lambda (x) (+ x 1)) 5)", "parse")
+//tree = BMLParser.matchAll("((lambda (x y) (- x y)) 5 6)", "parse")
+//tree = BMLParser.matchAll("((lambda (n) (if (= n 0) 1 (* n (thisFunction (- n 1))))) 5)", "parse")
+//tree = BMLParser.matchAll("(((lambda (x) (lambda () x)) 5))", "parse")
+//tree = BMLParser.matchAll("(((lambda (x) (lambda (y) (- x y))) 5) 6)", "parse")
+//tree = BMLParser.matchAll("((((lambda (x) (lambda (y) (lambda () (+ x y)))) 5) 6))", "parse")
+//tree = BMLParser.matchAll("(((lambda (x y) (lambda () (+ x (+ y x))))) 5 6))", "parse")
+//tree = BMLParser.matchAll("((((lambda (x) (lambda (y) (lambda () (+ x y)))) 5) 6))", "parse")
+//tree = BMLParser.matchAll("(((lambda (x y) (lambda () (+ x y))) 5 6))", "parse")
+//tree = BMLParser.matchAll("(((lambda (x y) (lambda () (+ x (+ y x)))) 5 6))", "parse")
+tree = BMLParser.matchAll("(((((lambda (x) (lambda (y) (lambda (z) (lambda () (+ x (+ y z)))))) 1) 2) 3))", "parse")
+
+code = BMLCompiler.match(tree, "compile")
+
--- gc.c
+++ gc.c
@@ -4,15 +4,24 @@
 #include <string.h>
 #include <assert.h>
 
-// TODO: add IJZ, IEQ, INEQ, ...
-// TODO: make closures work
+// Stack Layout:       ... local vars, etc.
+//               fp -> oldFp
+//                     oldIbp
+//                     oldIp
+//                     nArgs
+//                     arg(n)
+//                     arg(n - 1)
+//                     arg(n - 2)
+//                     ...
+//                     arg(1)
+//                     function, which is arg(0)
+
+#define DEBUG 1
 
 typedef unsigned char byte_t;
 typedef int           value_t;
 
-value_t nil, stack, sp, globals;
-
-#define DEBUG 1
+value_t nil, stack, sp, globals, ipb, ip, fp;
 
 #define allocate(N, T) ((T *) calloc(N, sizeof(T)))
 
@@ -22,7 +31,7 @@ value_t nil, stack, sp, globals;
 
 #define isOop(X)       (((X) & 1) == 0)
 #define Oop(X)         ((X) << 1)
-#define OopValue(X)    ({ value_t _x = X; if (DEBUG) assert(isOop(_x));     _x >> 1; })
+#define OopValue(X)    ({ value_t _x = X; if (DEBUG) assert(isOop(_x)); _x >> 1; })
 
 void error(char *fmt, ...) {
   va_list args;
@@ -41,11 +50,10 @@ typedef struct OTEntry {
   } ptr;
 } OTEntry;
 
+const size_t OrigOTSize = 1;
 size_t OTSize;
 OTEntry *OT, *freeList;
 byte_t *marked;
-
-const size_t OrigOTSize = 1;
 
 void growOT(void) {
   size_t  newOTSize    = OTSize > 0 ? OTSize * 2 : OrigOTSize;
@@ -80,10 +88,8 @@ int mark(value_t oop) {
   OTEntry e = OT[otIdx];
   value_t *slot = e.ptr.slots;
   int n = e.numSlots;
-  while (n--) {
-    r += mark(*slot);
-    slot++;
-  }
+  while (n--)
+    r += mark(*(slot++));
   return r;
 }
 
@@ -138,167 +144,123 @@ value_t mk(size_t numSlots) {
 #define mk2(a, b)    ({ value_t _r = mk(2); slotAtPut(_r, Int(0), a); slotAtPut(_r, Int(1), b); _r; })
 #define mk3(a, b, c) ({ value_t _r = mk(3); slotAtPut(_r, Int(0), a); slotAtPut(_r, Int(1), b); slotAtPut(_r, Int(2), c); _r; })
 
-#define addGlobal(V) ({ globals = mk2(nil, globals); slotAtPut(globals, Int(0), V); })
+void addGlobal(value_t v) {
+  slotAtPut(globals, Int(0), v);
+  globals = mk2(Int(0), globals);
+}
 
 void init(void) {
   OTSize = 0;
   growOT();
-  globals = nil = mk(0);
-  addGlobal(nil);
+  globals = mk2(Int(0), Int(0));
+  addGlobal(nil = mk(0));
   addGlobal(stack = mk(10240));
   sp = Int(0);
 }
 
-void _print(value_t v, int n) {
-  if (v == nil)
-    printf("nil");
-  else if (isInt(v))
-    printf("%d", IntValue(v));
-  else if (isOop(v)) {
-    printf("[");
-    if (n >= 5)
-      printf("...");
-    else
-      for (int i = 0; i < IntValue(numSlots(v)); i++) {
-        if (i > 0) printf(", ");
-        _print(slotAt(v, Int(i)), n + 1);
-      }
-    printf("]");
-  }
-  else
-    error("print doesn't know how to handle value (%d)\n", v);
+void _print(value_t v, value_t n, value_t selIdx) {
+  if      (v == nil) printf("nil");
+  else if (isInt(v)) printf("%d", IntValue(v));
+  else if (isOop(v)) { printf("[");
+                       if (IntValue(n) >= 5) printf("...");
+                       else                  for (int i = 0; i < IntValue(numSlots(v)); i++) {
+                                               if (i > 0) printf(", ");
+                                               if (i == IntValue(selIdx)) printf("<<<");
+                                               _print(slotAt(v, Int(i)), Int(IntValue(n) + 1), -1);
+                                               if (i == IntValue(selIdx)) printf(">>>");
+                                             }
+                       printf("]");
+                     }
+  else               error("print doesn't know how to handle value (%d)\n", v);
 }
 
-void print(value_t v)   { _print(v, 1); }
+void print(value_t v)   { _print(v, Int(1), Int(-1)); }
 void println(value_t v) { print(v); printf("\n"); }
 
-void printStack(void) {
-  printf("contents of stack: \n");
+void printState(void) {
+  printf("ipb(ip="); print(ip); printf("): "); _print(ipb, Int(1), ip); printf("\n");
+  printf("fp: "); println(fp);
+  printf("/---------------------------------------------------------------------\\\n");
   int spv = IntValue(sp);
-  while (spv > 0) {
-    spv--;
-    printf("  "); println(slotAt(stack, Int(spv)));
-  }
-  printf("that's it\n\n");
+  while (spv-- > 0) { printf("  "); println(slotAt(stack, Int(spv))); }
+  printf("\\---------------------------------------------------------------------/\n");
 }
 
-value_t ipb, ip, fp;
+#define Instruction(Name, Arg) value_t Name(int quiet, value_t Arg) { \
+                                 if (DEBUG && !quiet) { printf(">>> " #Name " "); println(Arg); }
 
-#define NewInstruction(Name, Arg) value_t Name(int isReal, value_t Arg) { \
-                                    if (isReal && DEBUG) { printf("\n>>> "); println(Arg); }
+Instruction(iPush, v)          slotAtPut(stack, sp, v);
+                               sp = Int(IntValue(sp) + 1);
+                               return v; }
 
-NewInstruction(iPush, v)
-  slotAtPut(stack, sp, v);
-  sp = Int(IntValue(sp) + 1);
-  return v;
-}
+Instruction(iPop, _)           assert(sp > Int(0));
+                               sp = Int(IntValue(sp) - 1);
+                               value_t r = slotAt(stack, sp);
+                               slotAtPut(stack, sp, Int(0));
+                               return r; }
 
-NewInstruction(iPeek, _)
-  assert(sp > Int(0));
-  return slotAt(stack, Int(IntValue(sp) - 1));
-}
+Instruction(iEq, _)            return iPush(1, Int(IntValue(iPop(1, nil)) == IntValue(iPop(1, nil)))); }
 
-NewInstruction(iPop, _)
-  assert(sp > Int(0));
-  sp = Int(IntValue(sp) - 1);
-  value_t r = slotAt(stack, sp);
-  slotAtPut(stack, sp, Int(0));
-  return r;
-}
+Instruction(iAdd, _)           value_t op2 = iPop(1, nil);
+                               return iPush(1, Int(IntValue(iPop(1, nil)) + IntValue(op2))); }
 
-NewInstruction(iAdd, _)
-  value_t op2 = iPop(0, nil);
-  return iPush(0, Int(IntValue(iPop(0, nil)) + IntValue(op2)));
-}
+Instruction(iSub, _)           value_t op2 = iPop(1, nil);
+                               return iPush(1, Int(IntValue(iPop(1, nil)) - IntValue(op2))); }
 
-NewInstruction(iSub, _)
-  value_t op2 = iPop(0, nil);
-  return iPush(0, Int(IntValue(iPop(0, nil)) - IntValue(op2)));
-}
+Instruction(iMul, _)           value_t op2 = iPop(1, nil);
+                               return iPush(1, Int(IntValue(iPop(1, nil)) * IntValue(op2))); }
 
-NewInstruction(iMul, _)
-  value_t op2 = iPop(0, nil);
-  return iPush(0, Int(IntValue(iPop(0, nil)) * IntValue(op2)));
-}
+Instruction(iBox, _)           return iPush(1, mk1(iPop(1, nil))); }
+Instruction(iUnbox, _)         return iPush(1, slotAt(iPop(1, nil), Int(0))); }
 
-NewInstruction(iBox, _)
-  return iPush(0, mk1(iPop(0, nil)));
-}
+Instruction(iLd, offset)       return iPush(1, slotAt(stack, Int(IntValue(fp) - IntValue(offset)))); }
 
-NewInstruction(iUnbox, _)
-  return iPush(0, slotAt(iPop(0, nil), Int(0)));
-}
+Instruction(iArg, n)           iLd(1, Int(4));
+                               int nArgs = IntValue(iPop(1, nil));
+                               return iLd(1, Int(5 + nArgs - IntValue(n))); }
 
-NewInstruction(iLd, offset)
-  if (isReal && DEBUG) { printf("\n>>> executing LD "); println(offset); }
-  return iPush(0, slotAt(stack, Int(IntValue(fp) - IntValue(offset))));
-}
+Instruction(iFv, n)            iLd(1, Int(4));
+                               int nArgs = IntValue(iPop(1, nil));
+                               iLd(1, Int(5 + nArgs));
+                               iUnbox(1, nil);
+                               value_t closure = iPop(1, nil);
+                               return iPush(1, slotAt(closure, Int(IntValue(n) + 1))); }
 
-NewInstruction(iArg, n)
-  iLd(0, Int(4));
-  int nArgs = IntValue(iPop(0, nil));
-  return iLd(0, Int(5 + nArgs - IntValue(n)));
-}
+Instruction(iMkFun, nFreeVars) value_t closure = mk(IntValue(nFreeVars) + 1);
+                               for (int i = IntValue(nFreeVars); i >= 0; i--)
+                                 slotAtPut(closure, Int(i), iPop(1, nil));
+                               return iPush(1, closure); }
 
-NewInstruction(iFv, n)
-  iLd(0, Int(4));
-  int nArgs = IntValue(iPop(0, nil));
-  iLd(0, Int(IntValue(5 + nArgs)));
-  iUnbox(0, nil);
-  value_t closure = iPop(0, nil);
-  return iPush(0, slotAt(closure, Int(IntValue(n) + 1)));
-}
+Instruction(iCall, nArgs)      iPush(1, slotAt(stack, Int(IntValue(sp) - 1 - IntValue(nArgs))));
+                               iUnbox(1, nil); // get the closure out of its "box"
+                               iUnbox(1, nil); // get the code out of the closure (it's in the 1st slot of the closure)
+                               value_t code = iPop(1, nil);
+                               iPush(1, nArgs);
+                               iPush(1, ip);
+                               iPush(1, ipb);
+                               iPush(1, fp);
+                               fp  = sp;
+                               ip  = Int(-1);
+                               ipb = code;
+                               return nil; }
 
-NewInstruction(iMkFun, nFreeVars)
-  value_t closure = mk(IntValue(nFreeVars) + 1);
-  for (int i = IntValue(nFreeVars); i >= 0; i--)
-    slotAtPut(closure, Int(i), iPop(0, nil));
-  return iPush(0, closure);
-}
+Instruction(iRet, _)           value_t r = iPop(1, nil);
+                               sp    = fp;
+                               fp    = iPop(1, nil);
+                               ipb   = iPop(1, nil);
+                               ip    = iPop(1, nil);
+                               int n = IntValue(iPop(1, nil));
+                               while (n-- >= 0) iPop(1, nil); // pop nArgs and function
+                               return iPush(1, r); }
 
-NewInstruction(iCall, nArgs)
-  iPush(0, slotAt(stack, Int(IntValue(sp) - 1 - IntValue(nArgs))));
-  iUnbox(0, nil); // get the closure out of its "box"
-  iUnbox(0, nil); // get the code out of the closure (it's in the 1st slot of the closure)
-  value_t code = iPop(0, nil);
-  iPush(0, nArgs);
-  iPush(0, ip);
-  iPush(0, ipb);
-  iPush(0, fp);
-  fp  = sp;
-  ip  = Int(-1);
-  ipb = code;
-  return nil;
-}
+Instruction(iJmp, n)           ip = Int(IntValue(ip) + IntValue(n)); return nil; }
+Instruction(iJz,  n)           return IntValue(iPop(1, nil)) == 0 ? iJmp(1, n) : nil; }
+Instruction(iJnz, n)           return IntValue(iPop(1, nil)) != 0 ? iJmp(1, n) : nil; }
 
-NewInstruction(iRet, _)
-  value_t r = iPop(0, nil);
-  sp    = fp;
-  fp    = iPop(0, nil);
-  ipb   = iPop(0, nil);
-  ip    = iPop(0, nil);
-  int n = IntValue(iPop(0, nil));
-  while (n-- > 0) iPop(0, nil);
-  printf("the function was "); println(iPop(0, nil)); // the function
-  return iPush(0, r);
-}
-
-NewInstruction(iJnz, n)
-  value_t v = iPop(0, nil);
-  if (IntValue(v) != 0)
-    ip = Int(IntValue(ip) + IntValue(n));
-  return nil;
-}
-
-NewInstruction(iHalt, _)
- printStack();
- exit(0);
-}
-
-enum                                   {IRET, ILD, IARG, IADD, ISUB, IMUL, IMKFUN, ICALL, IPUSH, IPOP, IBOX, IUNBOX,
-                                        ISLOTAT, ISLOTATPUT, IJNZ, IHALT, IFV};
-value_t (*jumpTable[])(int, value_t) = {iRet, iLd, iArg, iAdd, iSub, iMul, iMkFun, iCall, iPush, iPop, iBox, iUnbox,
-                                        NULL,    NULL,       iJnz, iHalt, iFv};
+enum
+  {oRet, oLD, oArg, oFV, oAdd, oSub, oMul, oMkFun, oCall, oPush, oPop, oBox, oUnbox, oEq, oJmp, oJZ, oJNZ, oHalt};
+value_t (*jumpTable[])(int, value_t) =
+  {iRet, iLd, iArg, iFv, iAdd, iSub, iMul, iMkFun, iCall, iPush, iPop, iBox, iUnbox, iEq, iJmp, iJz, iJnz};
 
 void interp(value_t prog) {
   fp  = sp;
@@ -306,33 +268,33 @@ void interp(value_t prog) {
   ip  = Int(0);
   while (1) {
     value_t instr = slotAt(ipb, ip), opcode = IntValue(slotAt(instr, Int(0))), op = slotAt(instr, Int(1));
-    if (DEBUG) printStack();
-    if (0 <= opcode && opcode < sizeof(jumpTable) / sizeof(value_t (*)(int, value_t)))
-      jumpTable[opcode](1, op);
-    else
-      error("unrecognized instruction (opcode = %d)\n", opcode);
+    if (DEBUG) { printf("\n\n\n"); printState(); printf("\n"); }
+    if (opcode == oHalt)                                                                    { if (DEBUG) printf(">>> iHalt\n"); }
+    else if (0 <= opcode && opcode < sizeof(jumpTable) / sizeof(value_t (*)(int, value_t))) jumpTable[opcode](0, op);
+    else                                                                                    error("invalid opcode %d\n", opcode);
     ip = Int(IntValue(ip) + 1);
+    if (DEBUG) { printf("\n"); printState(); printf("\n\n\n"); }
+    if (opcode == oHalt)
+      break;
   }
 }
 
-#define LD(O)    mk2(Int(ILD),    Int(O))
-
-#define PUSH(X)  mk2(Int(IPUSH),  Int(X))
-
-#define MKFUN(N) mk2(Int(IMKFUN), Int(N))
-#define CALL(N)  mk2(Int(ICALL),  Int(N))
-#define RET      mk2(Int(IRET),   nil)
-#define ARG(N)   mk2(Int(IARG),   Int(N))
-#define LARG(N)  mk2(Int(ILARG),  Int(N))
-#define FV(O)    mk2(Int(IFV),    Int(O))
-#define LFV(O)   mk2(Int(ILFV),   Int(O))
-#define HALT     mk2(Int(IHALT),  nil)
-#define ADD      mk2(Int(IADD),   nil)
-#define SUB      mk2(Int(ISUB),   nil)
-#define MUL      mk2(Int(IMUL),   nil)
-#define BOX      mk2(Int(IBOX),   nil)
-#define UNBOX    mk2(Int(IUNBOX), nil)
-#define JNZ(O)   mk2(Int(IJNZ),   Int(O))
+#define Push(X)  mk2(Int(oPush),  Int(X))
+#define MkFun(N) mk2(Int(oMkFun), Int(N))
+#define Call(N)  mk2(Int(oCall),  Int(N))
+#define Ret      mk2(Int(oRet),   nil)
+#define Arg(N)   mk2(Int(oArg),   Int(N))
+#define FV(O)    mk2(Int(oFV),    Int(O))
+#define Halt     mk2(Int(oHalt),  nil)
+#define Eq       mk2(Int(oEq),    nil)
+#define Add      mk2(Int(oAdd),   nil)
+#define Sub      mk2(Int(oSub),   nil)
+#define Mul      mk2(Int(oMul),   nil)
+#define Box      mk2(Int(oBox),   nil)
+#define Unbox    mk2(Int(oUnbox), nil)
+#define Jmp(O)   mk2(Int(oJmp),   Int(O))
+#define JZ(O)    mk2(Int(oJZ),    Int(O))
+#define JNZ(O)   mk2(Int(oJNZ),   Int(O))
 
 /* Print all contents in the object table. */
 void printOT() {
@@ -361,37 +323,68 @@ int main(void) {
 
 /*
   for (int i = 0; i < 100; i++) mk(1);
-  iPush(0, mk2(Int(4), nil));
+  iPush(1, mk2(Int(4), nil));
   for (int i = 0; i < 100; i++) mk(1);
-  iPush(0, mk2(Int(3), iPop(0, nil)));
+  iPush(1, mk2(Int(3), iPop(1, nil)));
   for (int i = 0; i < 100; i++) mk(1);
-  iPush(0, mk2(Int(2), iPop(0, nil)));
+  iPush(1, mk2(Int(2), iPop(1, nil)));
   for (int i = 0; i < 100; i++) mk(1);
-  iPush(0, mk2(Int(1), iPop(0, nil)));
+  iPush(1, mk2(Int(1), iPop(1, nil)));
   for (int i = 0; i < 100; i++) mk(1);
-  printStack();
+  printState();
 */
 
-  value_t add = mk(6);
-  addGlobal(add);
-  slotAtPut(add,  Int(0), ARG(1));
-  slotAtPut(add,  Int(1), UNBOX);
-  slotAtPut(add,  Int(2), ARG(2));
-  slotAtPut(add,  Int(3), UNBOX);
-  slotAtPut(add,  Int(4), ADD);
-  slotAtPut(add,  Int(5), RET);
-
-  value_t prog = mk(9);
-  addGlobal(prog);
-  slotAtPut(prog, Int(0), mk2(Int(IPUSH), add));
-  slotAtPut(prog, Int(1), BOX);
-  slotAtPut(prog, Int(2), MKFUN(0));
-  slotAtPut(prog, Int(3), PUSH(3));
-  slotAtPut(prog, Int(4), BOX);
-  slotAtPut(prog, Int(5), PUSH(4));
-  slotAtPut(prog, Int(6), BOX);
-  slotAtPut(prog, Int(7), CALL(2));
-  slotAtPut(prog, Int(8), HALT);
+ value_t l1 = mk(9);
+addGlobal(l1);
+slotAtPut(l1, Int(0), FV(0));
+slotAtPut(l1, Int(1), Unbox);
+slotAtPut(l1, Int(2), FV(1));
+slotAtPut(l1, Int(3), Unbox);
+slotAtPut(l1, Int(4), FV(2));
+slotAtPut(l1, Int(5), Unbox);
+slotAtPut(l1, Int(6), Add);
+slotAtPut(l1, Int(7), Add);
+slotAtPut(l1, Int(8), Ret);
+value_t l2 = mk(6);
+addGlobal(l2);
+slotAtPut(l2, Int(0), mk2(Int(oPush), l1));
+slotAtPut(l2, Int(1), FV(0));
+slotAtPut(l2, Int(2), FV(1));
+slotAtPut(l2, Int(3), Arg(1));
+slotAtPut(l2, Int(4), MkFun(3));
+slotAtPut(l2, Int(5), Ret);
+value_t l3 = mk(5);
+addGlobal(l3);
+slotAtPut(l3, Int(0), mk2(Int(oPush), l2));
+slotAtPut(l3, Int(1), FV(0));
+slotAtPut(l3, Int(2), Arg(1));
+slotAtPut(l3, Int(3), MkFun(2));
+slotAtPut(l3, Int(4), Ret);
+value_t l4 = mk(4);
+addGlobal(l4);
+slotAtPut(l4, Int(0), mk2(Int(oPush), l3));
+slotAtPut(l4, Int(1), Arg(1));
+slotAtPut(l4, Int(2), MkFun(1));
+slotAtPut(l4, Int(3), Ret);
+value_t prog = mk(17);
+addGlobal(prog);
+slotAtPut(prog, Int(0), mk2(Int(oPush), l4));
+slotAtPut(prog, Int(1), MkFun(0));
+slotAtPut(prog, Int(2), Box);
+slotAtPut(prog, Int(3), Push(1));
+slotAtPut(prog, Int(4), Box);
+slotAtPut(prog, Int(5), Call(1));
+slotAtPut(prog, Int(6), Box);
+slotAtPut(prog, Int(7), Push(2));
+slotAtPut(prog, Int(8), Box);
+slotAtPut(prog, Int(9), Call(1));
+slotAtPut(prog, Int(10), Box);
+slotAtPut(prog, Int(11), Push(3));
+slotAtPut(prog, Int(12), Box);
+slotAtPut(prog, Int(13), Call(1));
+slotAtPut(prog, Int(14), Box);
+slotAtPut(prog, Int(15), Call(0));
+slotAtPut(prog, Int(16), Halt);
 
   interp(prog);
   return 0;
